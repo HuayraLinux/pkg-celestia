b0cd73ff7f14c9a6c92fc560aaad35493274e6e2
Ignacio Losiggio <iglosiggio@gmail.com>
2018-03-09 12:07:22 -0300

    Made a better update_pot.sh script, updated patches accordingly

--- debian/update_pot.sh
+++ debian/update_pot.sh
@@ -1,7 +1,30 @@
 #!/bin/bash
 
-extractrc $(find -name \*.ui -o -name \*.rc -o -name \*.kcfg) > testrc.cpp
-xgettext -ki18n -k_ -F -j \
+# Extract catalog data
+grep \"Sol/Earth\"                    data/locations/earth_locs.ssc     | awk -F\" '{ print "_(\"" $2 "\");" }' > data/data.cpp
+grep \"Sol/Earth\"                    data/locations/world-capitals.ssc | awk -F\" '{ print "_(\"" $2 "\");" }' >> data/data.cpp
+egrep '^(\"|ReferencePoint)'          catalogs/solarsys.ssc  | awk -F'\"|:' '{ print "_(\"" $2 "\");" }' >> data/data.cpp
+egrep '\"Milky Way\"|\"LMC\"|\"SMC\"' catalogs/galaxies.dsc  | awk -F\"     '{ print "_(\"" $2 "\");" }' >> data/data.cpp
+egrep '\"Solar System Barycenter\"'   catalogs/nearstars.stc | awk -F\"     '{ print "_(\"" $2 "\");" }' >> data/data.cpp
+
+# Extract kde resource data
+extractrc src/celestia/kde/*/*.rc > src/celestia/kde/rc.cpp
+extractrc src/celestia/kde/*.ui >> src/celestia/kde/rc.cpp
+
+# Extract windows resource data
+# for some reason the windows resources are on the upstream repo, so we use an alternative file
+po/extract_resource_strings.pl < src/celestia/win32/res/celestia.rc > src/celestia/win32/res/resource_strings.new.cpp
+
+# Extract constellations data
+egrep ^\" catalogs/asterisms.dat | awk -F\" '{ print "_(\"" $2 "\");" }' > data/constellations.cpp
+
+xgettext --from-code utf-8 -k_ -kN_ -ki18n -kI18N_NOOP -ktr -F \
 	 -d celestia --package-name=celestia --package-version=1.7.0 --msgid-bugs-address=chris@teyssier.org --copyright-holder="Chris Laurel" \
-	 $(find . -name \*.c -o -name \*.cpp -o -name \*.cc -o -name \*.h -o -name \*.hpp -o -name \*.qml) -o po/celestia.pot
-rm testrc.cpp
+	 $(find src -name \*.c -o -name \*.cpp -o -name \*.cc -o -name \*.h -o -name \*.hpp -o -name \*.qml) data/data.cpp -o po/celestia.pot
+
+xgettext --from-code utf-8 -k_ -kK_ -ki18n -kI18N_NOOP -ktr \
+	 -d celestia_constellations --package-name=celestia --package-version=1.7.0 --msgid-bugs-address=chris@teyssier.org --copyright-holder="Chris Laurel" \
+	 data/constellations.cpp --from-code utf-8 -o po2/celestia_constellations.pot
+
+# Delete extracted strings
+rm -f data/data.cpp data/constellations.cpp src/celestia/kde/rc.cpp src/celestia/win32/res/resource_strings.new.cpp
